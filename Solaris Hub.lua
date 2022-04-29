25277a151d19fd16f864f80bd647c15250537a1a
Real_PainNonsense <98066737+RealPainNonsense@users.noreply.github.com>
2022-04-29 22:02:37 +0800

    Delete Solaris Hub.lua

--- Solaris Hub.lua
+++
@@ -1,8 +0,0 @@
-_G.Themes = "" --// insert a theme before Execute or else script won't load.
-_G.UILoadingSongID = ""
-repeat
-wait();
-until game:IsLoaded();
-pcall(function()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/RealPainNonsense/Solaris-Hub-V2/main/Solaris%20HubV2.lua', true))()
-end)
